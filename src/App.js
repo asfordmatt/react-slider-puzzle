81adeb0ac7628f2d4633ae14c1d3cfee639ee4d0
Matt Chapman <mchapman@uk.ibm.com>
2025-06-23 21:03:10 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Board from "./Board";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          Slider Puzzle
-          <Board/>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Slider Puzzle
+        <Board/>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/Board.jsx
+++ src/Board.jsx
@@ -1,82 +1,60 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Box from "./Box";
-import BoardLogic from "./BoardLogic";
-class Board extends Component {
-    static defaultProps = {
-        size: 3,
-        onMove: (i, j) => { console.log(`Clicked tile ${i},${j}`) }
+import React, { useState } from 'react';
+import Box from './Box';
+import BoardLogic from './BoardLogic';
+
+const boardLogic = new BoardLogic(3);
+const initialBoard = boardLogic.scramble()
+
+const Board = () => {
+  const [board, setBoard] = useState(initialBoard)
+  const [moves, setMoves] = useState(0)
+  const [isWin, setIsWin] = useState(false)
+
+  const onMove = (i, j) => {
+    console.log(`Clicked tile ${i},${j}`)
+  }
+
+  const newGame = () => {
+    setBoard(boardLogic.scramble());
+    setMoves(0);
+    setIsWin(boardLogic.checkWin());
+  };
+
+  const move = (i, j) => {
+    if (isWin) return;
+
+    if (boardLogic.move(i, j)) {
+      onMove(i, j);
+      setBoard(boardLogic.matrix);
+      setMoves(moves + 1);
+      setIsWin(boardLogic.checkWin());
     }
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = this.initialGameState();
-    }
+  /**
+   * returns a single slider row given the row data
+   * @param {Object} rowData row data
+   * @param {Number} i row number
+   */
+  const getRow = (rowData, j) => {
+    return (
+      <div key={j} >
+        {rowData.map((bNum, i) => <Box key={bNum} boxNumber={bNum} onClick={() => move(i, j)} />)}
+      </div>
+    );
+  }
 
-    initialGameState = () => {
-        this.boardLogic = new BoardLogic(this.props.data || this.props.size);
-        console.log(this.props.data);
-        console.log(this.props.data ? this.boardLogic.matrix : this.boardLogic.scramble());
-        return {
-            board: this.props.data ? this.boardLogic.matrix : this.boardLogic.scramble(),
-            moves: 0,
-            isWin: this.boardLogic.checkWin()
-        }
-    };
-
-    //note declaring class function as an arrow function gives us automatic 'this' binding.
-    move = (i, j) => {
-        if (this.state.isWin)
-            return;
-
-        if (this.boardLogic.move(i, j)) {
-            this.props.onMove(i, j);
-            this.setState((prevState) => ({
-                board: this.boardLogic.matrix,
-                moves: prevState.moves + 1,
-                isWin: this.boardLogic.checkWin(),
-            }));
-        }
-    }
-
-    /**
-     * returns a single slider row given the row data
-     * @param {Object} rowData row data
-     * @param {Number} i row number
-     */
-    getRow = (rowData, j) => {
-        return (
-            <div key={j} >
-                {rowData.map((bNum, i) => <Box key={bNum} boxNumber={bNum} onClick={() => this.move(i, j)} />)}
-            </div>
-        );
-    }
-
-    newGame = () => {
-        this.setState(this.initialGameState());
-    }
-
-    render() {
-        let rows = this.state.board.map(this.getRow);
-        let message = (this.state.isWin ? "Winner !!! " : "Total ") + `Moves: ${this.state.moves}`;
-        return (
-            <div className="slider-board">
-                {rows}
-                <span className="slider-msg">
-                    {message}
-                </span>
-                <div className="btn-new-game">
-                    <button onClick={this.newGame}>New Game</button>
-                </div>
-            </div>
-        );
-    }
+  return (
+    <div className="slider-board">
+      {board.map(getRow)}
+      <span className="slider-msg">
+        {(isWin ? "Winner !!! " : "Total ") + `Moves: ${moves}`}
+      </span>
+      <div className="btn-new-game">
+        <button onClick={newGame}>New Game</button>
+      </div>
+    </div>
+  );
 }
-
-Board.propTypes = {
-    data: PropTypes.array,
-    size: PropTypes.number,
-    onMove: PropTypes.func
-};
 
 export default Board;
